195214d191472a3b8daf328d08d713326674e0dc
Axler <axlerchin7226@gmail.com>
2026-04-27 19:52:33 +0800

    Add more accepted units

--- units.py
+++ units.py
@@ -1,6 +1,49 @@
 # Single source of truth for the assignment's accepted units
 ACCEPTED_UNITS: list[str] = [
-    "cups", "pound", "ounces", "g", "ml", "inch", "teaspoons",
-    "minutes", "cup", "min", "mililitre", "gram", "minute", "second",
-    "tablespoon"
+    # Volume
+    "cup", "cups", "c", 
+    "teaspoon", "teaspoons", "tsp", "t",
+    "tablespoon", "tablespoons", "tbsp", 
+    "fluid ounce", "fluid ounces", "fl oz",
+    "pint", "pints", "pt",
+    "quart", "quarts", "qt",
+    "gallon", "gallons", "gal",
+    "milliliter", "milliliters", "millilitre", "millilitres", "mililitre", "ml",
+    "liter", "liters", "litre", "litres", "l",
+    
+    # Mass/Weight
+    "pound", "pounds", "lb", "lbs",
+    "ounce", "ounces", "oz",
+    "gram", "grams", "g",
+    "kilogram", "kilograms", "kg",
+    "milligram", "milligrams", "mg",
+    
+    # Length
+    "inch", "inches", "in",
+    "centimeter", "centimeters", "cm",
+    "millimeter", "millimeters", "mm",
+    
+    # Time
+    "second", "seconds", "sec", "secs", "s",
+    "minute", "minutes", "min", "mins", "m",
+    "hour", "hours", "hr", "hrs", "h",
+    
+    # Non-standard / Others
+    "pinch", "pinches", 
+    "dash", "dashes",
+    "drop", "drops",
+    "handful", "handfuls",
+    "clove", "cloves",
+    "bunch", "bunches",
+    "sprig", "sprigs",
+    "slice", "slices",
+    "piece", "pieces",
+    "stick", "sticks",
+    "head", "heads",
+    "stalk", "stalks",
+    "bag", "bags",
+    "box", "boxes",
+    "can", "cans",
+    "jar", "jars",
+    "packet", "packets"
 ]
